643b109626e3ceb3d004aa106c8e796ad3c39356
SnakeOPM <snikersdaun@gmail.com>
2023-07-25 03:28:45 +1000

    Started ochid admin implementation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,15 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('last_name');
+            $table->string('last_name')->nullable();
             $table->string('middle_name')->nullable();
             $table->text('description')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone')->unique();
+            $table->string('phone')->unique()->nullable();
             $table->string('password');
-            $table->smallInteger('user_type_id');
-            $table->unsignedBigInteger('party_id');
+            $table->smallInteger('user_type_id')->nullable();
+            $table->unsignedBigInteger('party_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
