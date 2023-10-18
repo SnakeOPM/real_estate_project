1de84822c618c5b402f0d20b28493a96f377f066
SnakeOPM <snikersdaun@gmail.com>
2023-10-18 17:58:02 +1000

    added new template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,14 +10,56 @@
 </head>
 <body>
 <div class="container">
-    <div class="row">
-        <nav>
-            <ul>
-                <li><a href="">Contacts</a></li>
-                <li><a href="">About</a></li>
-            </ul>
-        </nav>
-    </div>
+
+    <nav class="navbar navbar-expand-lg bg-white">
+        <div class="container">
+            <div class="navbar-translate">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation" data-target="#pagesNav">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="pagesNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="https://material-blog-pro-laravel.creative-tim.com" class="nav-link"><strong>Homepage</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/travel" class="nav-link"><strong>Travel</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/food" class="nav-link"><strong>Food</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/technology" class="nav-link"><strong>Technology</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/fashion" class="nav-link"><strong>Fashion</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/health" class="nav-link"><strong>Health</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://material-blog-pro-laravel.creative-tim.com/all_articles" class="nav-link"><strong>All Articles</strong></a>
+                    </li>
+                </ul>
+                <form action="https://material-blog-pro-laravel.creative-tim.com/search" class="form-inline ml-auto">
+                    <div class="form-group no-border nav-category-search bmd-form-group">
+                        <input type="text" class="form-control" name="searching" placeholder="Search">
+                    </div>
+                    <button type="submit" style="margin-right: 30px;" class="btn btn-white btn-just-icon btn-round">
+                        <i class="material-icons">search</i>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+
+
     @yield('content')
 </div>
 </body>
