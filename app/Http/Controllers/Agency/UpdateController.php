d9735317b629784a2d3582269736d44abfa6157e
SnakeOPM <snikersdaun@gmail.com>
2023-10-21 03:59:15 +1000

    completed Agency Service

--- app/Http/Controllers/Agency/UpdateController.php
+++ app/Http/Controllers/Agency/UpdateController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers\Agency;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Agency\CreateRequest;
 
+use App\Models\Agency;
 use Illuminate\Http\Request;
 
 class UpdateController extends BaseController
 {
-    public function __invoke(CreateRequest $request)
+    public function __invoke(CreateRequest $request, Agency $agency)
     {
         $data = $request->validated();
-        $this->service->update_agency($data);
+        $this->service->update_agency($data, $agency);
         return redirect()->route('agency.show');
 
     }
--- app/Services/Agency/Service.php
+++ app/Services/Agency/Service.php
@@ -2,7 +2,31 @@
 
 namespace App\Services\Agency;
 
+use App\Models\Agency;
+use App\Models\images;
+use Illuminate\Support\Facades\Storage;
+
 class Service
 {
+    public function update_agency($data, $agency)
+    {
+        $agency->update($data);
+    }
+
+    public function store($data, $file = null)
+    {
+        if ($file != null) {
+            Storage::disk('s3')->put('/images', $file);
+            Images::firstOrCreate($file->getClientOriginalName());
+            $data['image_id'] = Images::where('name', 'like', $file->getClientOriginalName())->first();
+        }
+        unset($data['images']);
+        Agency::create($data);
+    }
+
+    public function destroy_agency($id)
+    {
+        Agency::destroy($id);
+    }
 
 }
