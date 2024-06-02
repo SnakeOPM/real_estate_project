1c60cc930035022072cbba8d5c103657f61a63bb
SnakeOPM <snikersdaun@gmail.com>
2024-06-02 22:31:43 +1000

    some layout changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,45 +6,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Real estate</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!---@vite(['resources/sass/app.scss', 'resources/js/app.js'])-->
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
 </head>
 <body>
-<div class="container">
+<header class="container">
 
     <nav class="navbar navbar-expand-lg bg-white">
-        <div class="container">
-            <div class="navbar-translate">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false"
-                        aria-label="Toggle navigation" data-target="#pagesNav">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="navbar-toggler-icon"></span>
-                    <span class="navbar-toggler-icon"></span>
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
             <div class="collapse navbar-collapse" id="pagesNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
+                <nav class="navbar-nav">
                         <a href="{{route('home')}}"
-                           class="nav-link"><strong>Homepage</strong></a>
-                    </li>
-                    <li class="nav-item">
+                           class="nav-link">Homepage</a>
                         <a href="{{route('flat.index')}}"
-                           class="nav-link"><strong>Flats</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/food"
-                           class="nav-link"><strong>Agencies</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/technology"
-                           class="nav-link"><strong>Posts</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://material-blog-pro-laravel.creative-tim.com/category/fashion"
-                           class="nav-link"><strong>Parties</strong></a>
-                    </li>
-                </ul>
+                           class="nav-link">Flats</a>
+                        <a href="/"
+                           class="nav-link">Agencies</a>
+                        <a href="/"
+                           class="nav-link">Posts</a>
+                        <a href="{{route('party.index')}}"
+                           class="nav-link">Parties</a>
+                </nav>
                 <form action="https://material-blog-pro-laravel.creative-tim.com/search" class="form-inline ml-3">
                     <div class="form-group no-border nav-category-search bmd-form-group">
                         <input type="text" class="form-control" name="searching" placeholder="Search">
@@ -57,10 +38,10 @@
         </div>
     </nav>
 
+</header>
 
     <div class="container mt-8">
     @yield('content')
-</div>
 </div>
 </body>
 </html>
